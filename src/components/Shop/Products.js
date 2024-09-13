ece9c155d029607560994d93039e943e5056eb1d
borodkoleg <borodkoleg@gmail.com>
2024-09-13 18:00:18 +0300

    just a practice redux

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'my book',
+    description: 'the first book'
+  },
+  {
+    id: 'p2',
+    price: 7,
+    title: 'plugin',
+    description: 'the first plugin'
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(item => {
+          return <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        })}
       </ul>
     </section>
   );
