b5050511e269bad5acc2e9ebd0e22f39054918cf
borodkoleg <borodkoleg@gmail.com>
2024-09-16 19:15:46 +0300

    redux thunk, move code to cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -11,7 +12,7 @@ const cartSlice = createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find(item => item.id === newItem.id);
-      state.totalQuantity ++;
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -28,7 +29,7 @@ const cartSlice = createSlice({
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find(item => item.id === id);
-      state.totalQuantity --;
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter(item => item.id !== id);
       } else {
@@ -38,6 +39,43 @@ const cartSlice = createSlice({
     }
   }
 });
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending',
+      message: 'Sending cart data!',
+    }));
+
+    const sendRequest = async () => {
+      const response = await fetch('https://test-9fd9e-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed');
+      }
+    }
+
+    try {
+      await sendRequest();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfuly!',
+      }));
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: error.message,
+      }));
+    }
+  }
+}
 
 export const cartActions = cartSlice.actions;
 
